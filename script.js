719c019548e55920ffe68d4d7f643ef1ac0194ba
PD12121 <115214172+PD12121@users.noreply.github.com>
2023-07-04 18:18:28 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,2 +1,33 @@
-//your code here!
 
+const list = document.getElementById('infi-list');
+
+function addListItems() {
+ 
+  const numItemsToAdd = 2;
+
+  for (let i = 0; i < numItemsToAdd; i++) {
+    const itemList = document.createElement('li');
+    itemList.textContent = 'List Item';
+    list.appendChild(listItem);
+  }
+}
+
+
+function checkEndOfList() {
+  const listHeight = list.offsetHeight;
+  const scrollTop = list.scrollTop;
+  const scrollHeight = list.scrollHeight;
+
+  if (scrollTop + listHeight >= scrollHeight) {
+    addListItems();
+  }
+}
+
+
+for (let i = 0; i < 10; i++) {
+  const listItem = document.createElement('li');
+  listItem.textContent = 'List Item';
+  list.appendChild(listItem);
+}
+
+list.addEventListener('scroll', checkEndOfList);
